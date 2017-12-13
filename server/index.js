f2d9c6af6000afd4b2bbf018a1951e7c4eb155c2
Dheemanth6260 <dheemanthr@gmail.com>
2017-12-13 00:03:59 -0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+var express = require('express'),
+config = require('./config/config');    
+
+
+var logger = require('./config/logger');
+
+
+var app = express();    
+
+require('./config/express')(app, config);
+
+logger.log("Creating HTTP server on port: " + config.port);
+require('http').createServer(app).listen(config.port, function () {
+console.log("HTTP Server listening on port: " + config.port + ", in " + app.get('env') + " mode");
+});
+
+module.exports = app;
